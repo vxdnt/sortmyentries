67b5a1d9c40838d5c5d164339cebee8efc0d349c
vxdnt <vedantkonde36@gmail.com>
2024-08-20 17:40:11 +0530

    Update contact.js

--- contact.js
+++ contact.js
@@ -9,30 +9,3 @@ submitQueryButton.addEventListener('click', () => {
 contactUsButton.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
 });
-
-//HEADER AND FOOTER
-// Function to load the header
-function loadHeader() {
-    fetch('header.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('header-container').innerHTML = data;
-        })
-        .catch(error => console.error('Error loading header:', error));
-}
-
-// Function to load the footer
-function loadFooter() {
-    fetch('footer.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('footer-container').innerHTML = data;
-        })
-        .catch(error => console.error('Error loading footer:', error));
-}
-
-// Call the functions to load the header and footer when the page loads
-window.onload = function() {
-    loadHeader();
-    loadFooter();
-};
